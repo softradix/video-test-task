d6751ff30735c862dfe7f6523ad5650bb94b9c32
rohitchoudhary99 <rohit.choudhary@softradix.in>
2023-02-07 16:13:25 +0530

    code merge done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'facepop';
+  video: any;
+  button_text: string;
+  videoPlaying:boolean;
+  percentage: number;
+  ngOninit() {
+    this.video = <HTMLVideoElement>document.getElementById("video");
+  }
+  videoMute() {
+    // let video = <HTMLVideoElement>document.getElementById("video");
+    let mute_text = document.getElementById("mute_text");
+    console.log("mute_text", !this.video.muted)
+    this.video.muted = !this.video.muted;
+  }
+  videoPlay() {
+    let video = <HTMLVideoElement>document.getElementById("video");
+    let playButton = document.getElementById("play_button");
+    // Event listener for the play/pause button
+    playButton.addEventListener("click", function () {
+      if (video.paused == true) {
+        // Play the video
+        video.play();
+        // Update the button text to 'Pause'
+        playButton.innerHTML = "Pause";
+      } else {
+        // Pause the video
+        video.pause();
+        // Update the button text to 'Play'
+        playButton.innerHTML = "Play";
+      }
+    });
+    // if(!this.videoPlaying){
+    //   this.video.play();
+    //   this.videoPlaying = true;
+    // }else{
+    //   this.video.pause();
+    //   this.videoPlaying = false;
+    // }
+  }
+  onTimeUpdate(){
+    let video = <HTMLVideoElement>document.getElementById("video");
+    this.percentage = (video.currentTime / video.duration) * 100;
+  }
 }
+
+
+
+
+
+
+
